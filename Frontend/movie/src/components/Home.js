276664e590d96b9c70e93cefe9bba34182e08242
W2-80477 <satyamkumar.sk786@gmail.com>
2024-02-08 10:49:09 +0530

    home

--- Frontend/movie/src/components/Home.js
+++ Frontend/movie/src/components/Home.js
@@ -11,30 +11,30 @@ function Home() {
   return (
     <>
       <div className='carousel'>
-        <div id="carouselExampleIndicators" class="carousel slide">
-          <div class="carousel-indicators">
-            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+        <div id="carouselExampleIndicators" className="carousel slide">
+          <div className="carousel-indicators">
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
             <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
             <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
           </div>
-          <div class="carousel-inner">
-            <div class="carousel-item active">
-              <img src={landscapeHanuman} class="d-block w-50" alt="hanuman_pic" />
+          <div className="carousel-inner">
+            <div className="carousel-item active">
+              <img src={landscapeHanuman} className="d-block w-100" alt="hanuman_pic" />
             </div>
-            <div class="carousel-item">
-              <img src={landscapeAnimal} class="d-block w-50" alt="animal_pic" />
+            <div className="carousel-item">
+              <img src={landscapeAnimal} className="d-block w-100" alt="animal_pic" />
             </div>
-            <div class="carousel-item">
-              <img src={landscapeSalaar} class="d-block w-50" alt="salaar" />
+            <div className="carousel-item">
+              <img src={landscapeSalaar} className="d-block w-100" alt="salaar" />
             </div>
           </div>
-          <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Previous</span>
+          <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span className="visually-hidden">Previous</span>
           </button>
-          <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Next</span>
+          <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+            <span className="carousel-control-next-icon" aria-hidden="true"></span>
+            <span className="visually-hidden">Next</span>
           </button>
         </div>
 
@@ -42,32 +42,32 @@ function Home() {
 
 
       <div className='movie-card'>
-        <div class="row row-cols-1 row-cols-md-3 g-4">
-          <div class="col">
-            <div class="card h-20">
-              <img src={hanuman} class="card-img-top" alt="hanuman" />
-              <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <div className="row row-cols-1 row-cols-md-3 g-4">
+          <div className="col">
+            <div className="card h-100">
+              <img src={hanuman} className="card-img-top" alt="hanuman" />
+              <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+                <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
               </div>
             </div>
           </div>
 
-          <div class="col">
-            <div class="card h-50">
-              <img src={animal} class="card-img-top" alt="animal" />
-              <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">This is a short card.</p>
+          <div className="col">
+            <div className="card h-100">
+              <img src={animal} className="card-img-top" alt="animal" />
+              <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+                <p className="card-text">This is a short card.</p>
               </div>
             </div>
           </div>
-          <div class="col">
-            <div class="card h-50">
-              <img src={salaar} class="card-img-top" alt="salaar" />
-              <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">This is a short card.</p>
+          <div className="col">
+            <div className="card h-100">
+              <img src={salaar} className="card-img-top" alt="salaar" />
+              <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+                <p className="card-text">This is a short card.</p>
               </div>
             </div>
           </div>
