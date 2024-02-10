049f82dacbaa1d25cac0912000ec77368ed5a7db
W2-80477 <satyamkumar.sk786@gmail.com>
2024-02-11 00:06:10 +0530

    home page

--- Frontend/movie/src/components/Home.js
+++ Frontend/movie/src/components/Home.js
@@ -10,36 +10,32 @@ import landscapeSalaar from "../Image/landscapeSalaar.jpeg";
 function Home() {
   return (
     <>
-      <div className='carousel'>
-        <div id="carouselExampleIndicators" className="carousel_slide">
-          <div className="carousel-indicators">
-            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
-            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-          </div>
-          <div className="carousel-inner">
-            <div className="carousel-item active">
-              <img src={landscapeHanuman} className="d-block w-100" alt="hanuman_pic" />
-            </div>
-            <div className="carousel-item">
-              <img src={landscapeAnimal} className="d-block w-100" alt="animal_pic" />
-            </div>
-            <div className="carousel-item">
-              <img src={landscapeSalaar} className="d-block w-100" alt="salaar" />
-            </div>
-          </div>
-          <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span className="visually-hidden">Previous</span>
-          </button>
-          <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-            <span className="carousel-control-next-icon" aria-hidden="true"></span>
-            <span className="visually-hidden">Next</span>
-          </button>
+      <div id="carouselExampleIndicators" class="carousel slide">
+        <div class="carousel-indicators">
+          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
         </div>
-
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img src={landscapeHanuman} className="d-block w-100" alt="hanuman_pic" />
+          </div>
+          <div class="carousel-item">
+            <img src={landscapeAnimal} className="d-block w-100" alt="animal_pic" />
+          </div>
+          <div class="carousel-item">
+            <img src={landscapeSalaar} className="d-block w-100" alt="salaar" />
+          </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
       </div>
-
 
       <div className='movie-card'>
         <div className="row row-cols-1 row-cols-md-3 g-4">
@@ -74,6 +70,10 @@ function Home() {
         </div>
       </div>
     </>
+
+
+
+
   )
 }
 
--- Frontend/movie/src/components/Navbar.js
+++ Frontend/movie/src/components/Navbar.js
@@ -6,19 +6,48 @@ import logo from "../Image/logo.jpg";
 
 function Navbar() {
   return (
-    <div className='navbar'>
-        <img src={logo} alt='logo-movie'/>
-        <ul className='nav-manu'>
-        <Link to='/'><li>Home</li></Link>
-        <Link to='/signup'><li>SignUp</li></Link>
-        <Link to='/signin'><li>Signin</li></Link>
-        </ul>
-        <form class="d-flex" role="search">
+    <>
+ 
+    <nav class="navbar  navbar-expand-lg bg-dark" data-bs-theme="dark" >
+  <div class="container-fluid">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <a class="navbar-brand" href="#">Movie_Booking</a>
+    <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <Link class="nav-link active" aria-current="page" to ="/">Home</Link>
+        </li>
+        <li class="nav-item">
+          <Link class="nav-link active" aria-current="page" to="/signup">SignUp</Link>
+        </li>
+        <li class="nav-item">
+          <Link class="nav-link active" aria-current="page" to="/signin">SignIn</Link>
+        </li>
+      </ul>
+      <form class="d-flex" role="search">
         <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
         <button class="btn btn-outline-success" type="submit">Search</button>
       </form>
-      
     </div>
+  </div>
+</nav>
+
+    </>
+    // <div className='navbar'>
+    //     <img src={logo} alt='logo-movie'/>
+    //     <ul className='nav-manu'>
+    //     <Link to='/'><li>Home</li></Link>
+    //     <Link to='/signup'><li>SignUp</li></Link>
+    //     <Link to='/signin'><li>Signin</li></Link>
+    //     </ul>
+    //     <form class="d-flex" role="search">
+    //     <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+    //     <button class="btn btn-outline-success" type="submit">Search</button>
+    //   </form>
+      
+    // </div>
   )
 }
 
